5b1bd0ffdd968a575843e89dbf0c5c234b9796aa
Angela Ruiz <angela.ruiz.rodriguez.8@gmail.com>
2024-10-16 12:27:58 +0200

    Rutas , paginas y componentes

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+    <nav>
+        <li>Logo</li>
+        <li>barra</li>
+        <li>Favs</li>
+        <li>Cartbutton</li>
+        <li>profilebutton</li>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
