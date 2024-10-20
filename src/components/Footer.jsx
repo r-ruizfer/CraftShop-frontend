cb0f8672096ab1fcc0fd9973fc520b8a1e36b385
r.ruizfer <r.ruizfer97@gmail.com>
2024-10-21 00:33:26 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,88 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
 
-function Footer() {
+const Footer = () => {
   return (
-    <div id='footer'>
-      Footer
-    </div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-container">
+        <p>Connect with our Devs in:</p>
+        <div className="dev-socials">
+          <div>
+            <h4>Ruben Ruiz:</h4>
+            <div className="social-icons">
+              <a
+                href="https://github.com/r-ruizfer"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/ruben-ruiz-5b451630b/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+              </a>
+              <a
+                href="https://twitter.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+              </a>
+              <a
+                href="https://facebook.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faFacebook} size="2x" />
+              </a>
+            </div>
+          </div>
+          <div>
+            <h4>Angela Ruiz:</h4>
+            <div className="social-icons">
+              <a
+                href="https://github.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+              </a>
+              <a
+                href="https://linkedin.com/in/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+              </a>
+              <a
+                href="https://twitter.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+              </a>
+              <a
+                href="https://facebook.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faFacebook} size="2x" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
