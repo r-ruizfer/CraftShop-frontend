17218ba25ece262f14ea2b9f3662f217998596bc
r.ruizfer <r.ruizfer97@gmail.com>
2024-10-21 18:10:39 +0200

    merge ruben

--- src/components/OffCanvasProfile.jsx
+++ src/components/OffCanvasProfile.jsx
@@ -1,33 +1,92 @@
-import { useState } from "react";
-import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import { person } from "react-icons-kit/oct/person";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Icon } from "react-icons-kit";
+import service from "../services/config";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/auth.context";
+import { width } from "@fortawesome/free-brands-svg-icons/fa42Group";
+import { justify } from "@cloudinary/url-gen/qualifiers/textAlignment";
+import { background } from "@cloudinary/url-gen/qualifiers/focusOn";
+import { BackgroundColor } from "@cloudinary/url-gen/actions/background/actions/BackgroundColor";
 
 function OffCanvasProfile(props) {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const { user, isLoggedIn, authenticateUser } = useContext(AuthContext);
+  const [userProfile, setUserProfile] = useState(user);
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getUser = () => {
+      const storedToken = localStorage.getItem("authToken");
+
+      if (storedToken && isLoggedIn && user) {
+        if (user._id) {
+          service
+            .get(`/users/${user._id}`)
+            .then((response) => {
+              console.log(response);
+              setUserProfile(response.data);
+              setLoading(false);
+            })
+            .catch((err) => {
+              const errorDescription = err.response.data.message;
+              setErrorMessage(errorDescription);
+            });
+        } else {
+          setErrorMessage("User ID is not available.");
+          setLoading(false);
+        }
+      }
+    };
+    getUser();
+  }, [isLoggedIn, user]);
 
   return (
     <div>
       <Icon icon={person} size={15} onClick={handleShow} />
-      <Offcanvas show={show} onHide={handleClose} placement="end">
+      <Offcanvas
+        style={{ width: "200px" }}
+        show={show}
+        onHide={handleClose}
+        placement="end"
+      >
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title onClick={handleClose}>
-            <Link to="/profile/">View Profile</Link>
-          </Offcanvas.Title>
+          <Offcanvas.Title>Profile Options</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body onClick={handleClose}>
-          <li>{!props.isLoggedIn && <Link to="/signup"> Sign Up</Link>}</li>
-          <li>{!props.isLoggedIn && <Link to="/login">Log In</Link>}</li>
-          <li>
-            {props.isLoggedIn && (
-              <Link onClick={props.handleLogout}>Log Out</Link>
-            )}
-          </li>
+          {isLoggedIn && user ? (
+            <Link to="/profile/">
+              <img
+                src={userProfile.image}
+                alt="profile-photo"
+                className="offcanvas-profile-photo"
+              />
+              <p style={{ textDecoration: "none" }}>{userProfile.username}</p>
+              View Profile
+            </Link>
+          ) : null}
+
+          {!isLoggedIn && (
+            <Link to="/signup">
+              <li> Sign Up</li>
+            </Link>
+          )}
+          {!isLoggedIn && (
+            <Link to="/login">
+              <li>Log In</li>
+            </Link>
+          )}
+
+          {isLoggedIn && (
+            <Link onClick={props.handleLogout}>
+              <li>Log Out</li>
+            </Link>
+          )}
         </Offcanvas.Body>
       </Offcanvas>
     </div>
